a312ec94968c9af74197d0f848d87b8dd6a4a957
Ilyenna <jwelsh518@gmail.com>
2023-01-14 10:40:50 -0500

    Finished work on '.button' event listeners

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 const buttonsSelector = document.querySelectorAll('.button');
 const numberDisplay = document.querySelector('.number-display');
 
+// place an event listener onto each button
+// that has class '.button'
 buttonsSelector.forEach((element) => {
     element.addEventListener("click", () => {
         idSelector = element.getAttribute('id');
 
         switch (idSelector) {
             case 'negative':
+                // convert numberDisplay value to a number
+                let numberInDisplay = Number(numberDisplay.value);
+
+                // if it's greater than zero, convert it to negative
+                // else convert it to positive
+                if (numberInDisplay > 0) {
+                    numberInDisplay = -Math.abs(numberInDisplay);
+                } else {
+                    numberInDisplay = Math.abs(numberInDisplay);
+                }
+
+                numberDisplay.value = '';
+                numberDisplay.value += `${numberInDisplay.toString()}`;
                 break;
+
             case 'decimal':
                 numberDisplay.value += '.';
                 break;
+
             case '0':
                 if (numberDisplay.value == '') {
                     break;
                 }
+
             default:
                 numberDisplay.value += `${idSelector}`;
         } 
     });
 });
 
-
+// general 'operate' function
 function operate(operator, num1, num2) {
     switch (operator) {
         case '+':
@@ -35,7 +53,7 @@ function operate(operator, num1, num2) {
     }
 }
 
-
+// basic arithmetic functions
 function add(num1, num2) {
     return num1 + num2;
 }
