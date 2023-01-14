93d8d8c5180741f8ad38b18fb539af88d3c1198e
Ilyenna <jwelsh518@gmail.com>
2023-01-14 10:10:43 -0500

    Took out event listeners, used forEach instead

--- script.js
+++ script.js
@@ -1,56 +1,12 @@
-const button1 = document.getElementById('1');
-const button2 = document.getElementById('2');
-const button3 = document.getElementById('3');
-const buttonMinus = document.getElementById('minus');
-
-const button4 = document.getElementById('4');
-const button5 = document.getElementById('5');
-const button6 = document.getElementById('6');
-const buttonMultiply = document.getElementById('multiply');
-
-const button7 = document.getElementById('7');
-const button8 = document.getElementById('8');
-const button9 = document.getElementById('9');
-const buttonDivide = document.getElementById('divide');
-
-const buttonNegative = document.getElementById('1');
-const button0 = document.getElementById('2');
-const buttonDecimal = document.getElementById('3');
-const buttonPlus = document.getElementById('plus');
-
-
+const buttonsSelector = document.querySelectorAll('.button');
 const numberDisplay = document.querySelector('.number-display');
 
-
-button1.addEventListener("click", ()=> {
-    numberDisplay.value += "1";
-});
-button2.addEventListener("click", ()=> {
-    numberDisplay.value += "2";
-});
-button3.addEventListener("click", ()=> {
-    numberDisplay.value += "3";
+buttonsSelector.forEach((element) => {
+    element.addEventListener("click", () => {
+        numberDisplay.value += `${element.getAttribute('id')}`;
+    });
 });
 
-button4.addEventListener("click", ()=> {
-    numberDisplay.value += "4";
-});
-button5.addEventListener("click", ()=> {
-    numberDisplay.value += "5";
-});
-button6.addEventListener("click", ()=> {
-    numberDisplay.value += "6";
-});
-
-button7.addEventListener("click", ()=> {
-    numberDisplay.value += "7";
-});
-button8.addEventListener("click", ()=> {
-    numberDisplay.value += "8";
-});
-button9.addEventListener("click", ()=> {
-    numberDisplay.value += "9";
-});
 
 function operate(operator, num1, num2) {
     switch (operator) {
